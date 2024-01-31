645621f40880249c7fb8b169d9da437da48662e2
geunsu <geunsu23@naver.com>
2024-02-01 05:23:52 +0900

    refactor:코드 간결화

--- BJ/9655.py
+++ BJ/9655.py
@@ -1,18 +1,6 @@
 n = int(input())
-answer = [-1]*1001
 
-# 0 => 상근(SK) / 1=> 창영(CY)
-answer[1] = 0
-answer[2] = 1
-answer[3] = 0
-
-for i in range(4,n+1):
-    if answer[i-1] == 1:
-        answer[i] = 0
-    else:
-        answer[i] = 1
-
-if answer[n] == 0:
-    print("SK")
-else:
+if n%2 == 0:
     print("CY")
+else:
+    print("SK")
